d7fac5e375b4a0859237f5ff88b2be218b311337
RickyNJ <rickynijsten@gmail.com>
2024-11-04 10:29:49 +0100

    init

---
+++ token/token.c
@@ -0,0 +1,25 @@
+// token.c
+
+const char ILLEGAL[] = "ILLEGAL";
+const char END_OF_FILE[] = "EOF";
+
+// identifiers + literals
+const char IDENT[] = "IDENT";
+const char INT[] = "INT";
+
+// operators
+const char ASSIGN[] = "=";
+const char PLUS[] = "+";
+
+// delimiters
+const char COMMA[] = ",";
+const char SEMICOLON[] = ";";
+
+const char LPAREN[] = "(";
+const char RPAREN[] = ")";
+const char LBRACE[] = "{";
+const char RBRACE[] = "}";
+
+// keywords
+const char FUNCTION[] = "FUNCTION";
+const char LET[] = "LET";
